75ce97f89252d04c507d34d6e4d7a64c2cc3b892
Aaron Anderson <aaron.anderson74@yahoo.com>
2015-10-28 16:20:26 -0500

    initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nexradaws',
+    version='1.0',
+    packages=['nexradaws'],
+    url='https://github.com/aarande/nexradaws',
+    license='MIT',
+    author='Aaron Anderson',
+    author_email='',
+    description='This python package is designed to provide an interface to query and download NEXRAD archive data available on Amazon Web Services'
+)
