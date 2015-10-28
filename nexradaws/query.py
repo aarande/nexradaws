f3b884ba526e9df34de7c67c6203d581adf453b5
Aaron Anderson <aaron.anderson74@yahoo.com>
2015-10-28 16:31:15 -0500

    doc strings

--- nexradaws/query.py
+++ nexradaws/query.py
@@ -13,6 +13,11 @@ class NexradQuery(object):
         self.bucket = self.s3conn.get_bucket('noaa-nexrad-level2')
 
     def get_available_years(self):
+        """
+        This method allows you to get the years that are currently available.
+        :rtype : list
+        :return: A list of strings representing the years available
+        """
         years = []
         resp = list(self.bucket.list("","/"))
         for each in resp:
@@ -22,6 +27,12 @@ class NexradQuery(object):
         return years
 
     def get_available_months(self,year):
+        """
+        This method allows you to get the available months in a given year.
+        :rtype : list
+        :param year: string - the year we are requesting available months for (i.e. 2010)
+        :return: A list of strings representing the months available for that year
+        """
         months = []
         resp = list(self.bucket.list("%s/"%year,"/"))
         for each in resp:
@@ -31,6 +42,13 @@ class NexradQuery(object):
         return months
 
     def get_available_days(self,year,month):
+        """
+        This method allows you to get the available days in a given month and year.
+        :rtype : list
+        :param year: string - the year we are requesting available days for (i.e 2010)
+        :param month: string - the month we are requesting available days for (i.e. 05)
+        :return: A list of strings representing the days available in the given month and year
+        """
         days = []
         resp = list(self.bucket.list("%s/%s/"%(year,month),"/"))
         for each in resp:
@@ -40,6 +58,15 @@ class NexradQuery(object):
         return days
 
     def get_available_radars(self,year,month,day):
+        """
+        This method allows you to get the available radars in a given day, month, and year.
+        :rtype : list
+        :param year: string - the year we are requesting available radars for (i.e 2010)
+        :param month: string - the month we are requesting available radars for (i.e. 05)
+        :param day: string - the day we are requesting available radars for (i.e. 01)
+        :return: A list of string representing the radar sites available in the given
+        day, month, and year
+        """
         radars = []
         resp = list(self.bucket.list("%s/%s/%s/"%(year,month,day),"/"))
         for each in resp:
