6d33646f696c79c77be6b2f2b2805a7282f5cec7
Aaron Anderson <aarande@users.noreply.github.com>
2017-07-07 10:27:18 -0500

    nbsphinx_timout edit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'IPython.sphinxext.ipython_console_highlighting',
 ]
 
-nbsphinx_timeout = 360
+nbsphinx_timeout = 720
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
